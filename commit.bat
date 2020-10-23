fb46b98bf0e5099e396b852886e8bc2e2386ffa1
micheal_kinney <micheal_kinney@bitbucket.org>
2020-10-23 01:51:21 -0700

    Auto Commit #

--- commit.bat
+++ commit.bat
@@ -4,24 +4,27 @@
 :: Currently, the script can simi-auto commit
 :: by being passed a commit message either
 :: from cmmandline or prompt (when no message
-:: is provided)
+:: is provided).
 
 if "%~1"=="" (
+	:: Manual Message replaced w/ auto increment
 	:: set /p msg=Please provide a commit message... 
+	
 	git rev-list --all --count > %temp%\cnt.txt
 	set /p cnt=<%temp%\cnt.txt
-	set "msg=Auto Commit #%count%" 
+	set "msg=Auto Commit #%cnt%" 
 ) else (
+	:: Commit can take message as passthrough
 	set "msg=%1"
 )
 
-git add --all && git commit -m %msg% && git push
+git add --all && git commit -m "%msg%" && git push
 
 if NOT "%errorlevel%"=="0" (
-        :: "Fixes" the Repo if/when out of date
-        :: but still needs testing
+	:: "Fixes" the Repo if/when out of date
+	:: but still needs testing
 	git pull
-        git add --all && git commit -m %msg% && git push
+	git add --all && git commit -m %msg% && git push
 )
 
 :: Reset Message for Commandline
